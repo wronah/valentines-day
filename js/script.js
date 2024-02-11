3ea758d46ad943afecc89aef8c96ea90b5d59e61
wronah <hubertwro7@gmail.com>
2024-02-11 23:55:03 +0100

    added popup after clicking yes :)

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@ const btnNo = document.querySelector('#no')
 const letter = document.querySelector('.letter')
 const question = document.querySelector('.question')
 const counterDisplay = document.querySelector('#counter-display')
+const popup = document.querySelector('.popup')
+const closePopup = document.querySelector('.close-popup')
 
 const letterRect = letter.getBoundingClientRect();
 const btnNoRect = btnNo.getBoundingClientRect();
 
 let isOpen = false;
+let isVisible = false;
 
 frontFlap.addEventListener('click', (e) => {
     e.target.textContext = !isOpen ? open() : close();
@@ -23,6 +26,12 @@ frontPocket.addEventListener('click', (e) => {
     isOpen = !isOpen 
 })
 btnYes.addEventListener('click', () => {
+    !isVisible ? popup.classList.remove('hidden') : popup.classList.add('hidden')
+    isVisible = !isVisible
+})
+closePopup.addEventListener('click', () => {
+    !isVisible ? popup.classList.remove('hidden') : popup.classList.add('hidden')
+    isVisible = !isVisible
 })
 btnNo.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (letterRect.width - btnNoRect.width)) + 1
