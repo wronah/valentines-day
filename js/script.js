7d65a0a41147b82dbfe1588a98245cda7efc0d6a
wronah <hubertwro7@gmail.com>
2025-02-12 23:16:26 +0100

    add random messages and gifs, add confetti after clicking yes

--- js/script.js
+++ js/script.js
@@ -26,9 +26,25 @@ frontPocket.addEventListener('click', (e) => {
     isOpen = !isOpen 
 })
 btnYes.addEventListener('click', () => {
-    !isVisible ? popup.classList.remove('hidden') : popup.classList.add('hidden')
-    isVisible = !isVisible
-})
+    if (!isVisible) {
+        popup.classList.remove('hidden');
+        const randomHeadingText = getRandomElementWeighted(headingTexts);
+        const randomBottomText = getRandomElementWeighted(bottomTexts);
+        const randomGif = getRandomElementWeighted(gifs);
+        document.querySelector('#heading-text').innerHTML = randomHeadingText.text;
+        document.querySelector('#bottom-text').innerHTML = randomBottomText.text;
+        document.querySelector('.popup img').src = randomGif.src;
+
+        confetti({
+            particleCount: 300,
+            spread: 150,
+            origin: { y: 0.5 }
+        });
+    } else {
+        popup.classList.add('hidden');
+    }
+    isVisible = !isVisible;
+});
 closePopup.addEventListener('click', () => {
     !isVisible ? popup.classList.remove('hidden') : popup.classList.add('hidden')
     isVisible = !isVisible
@@ -57,4 +73,50 @@ function open() {
 function close() {
     envelope.classList.add('close')
     envelope.classList.remove('open')
+}
+
+const headingTexts = [
+    { text: "I LOVE YOU MY LITTLE GINGERBREAD", weight: 5 },
+    { text: "YOU ARE MY SWEETEST LOVE", weight: 2 },
+    { text: "MY HEART BEATS FOR YOU", weight: 2 },
+    { text: "FOREVER MY LOVE, FOREVER MY HOME", weight: 3 },
+    { text: "MY DARLING, MY EVERYTHING", weight: 1 },
+    { text: "HUGS, KISSES, AND ENDLESS LOVE", weight: 1 },
+    { text: "YOU ARE MY GREATEST GIFT", weight: 2 },
+    { text: "MY LOVE STORY BEGINS WITH YOU", weight: 3 },
+    { text: "MY HEART BELONGS TO YOU", weight: 1 },
+    { text: "YOU ARE MY SALLY", weight: 5 }
+];
+
+const bottomTexts = [
+    { text: "YOU COMPLETE ME IN EVERY WAY", weight: 3 },
+    { text: "YOU ARE THE BEST IN THE WORLD", weight: 5 },
+    { text: "YOU ARE MY HAPPINESS EVERY DAY", weight: 1 },
+    { text: "LIFE IS BEAUTIFUL BECAUSE OF YOU", weight: 5 },
+    { text: "MY LOVE FOR YOU IS ENDLESS", weight: 3 },
+    { text: "EVERY DAY WITH YOU IS A BLESSING", weight: 5 },
+    { text: "YOU ARE MY ONE AND ONLY", weight: 5 },
+    { text: "THANK YOU FOR BEING YOU", weight: 2 },
+    { text: "I LOVE YOU MORE THAN WORDS CAN SAY", weight: 2 },
+    { text: "THERE’S NO ONE ELSE LIKE YOU", weight: 1 }
+];
+
+const gifs = [
+    { src: "./gifs/mua.gif", weight: 10 },
+    { src: "./gifs/mewing.gif", weight: 1 },
+    { src: "./gifs/sally-mcqueen.gif", weight: 10 },
+    { src: "./gifs/kitty-love.gif", weight: 5 },
+    { src: "./gifs/patrick-star.gif", weight: 5 },
+    { src: "./gifs/love-you.gif", weight: 5 }
+];
+
+function getRandomElementWeighted(arr) {
+    const totalWeight = arr.reduce((sum, item) => sum + item.weight, 0);
+    let random = Math.random() * totalWeight;
+    for (const item of arr) {
+        if (random < item.weight) {
+            return item;
+        }
+        random -= item.weight;
+    }
 }
